d6c3bafbe6bfb4c9f4c9184fd5fbfd2c19c08e3d
Govindu <govinduthejana@gmail.com>
2025-03-09 18:18:48 +0530

    Update CORS configuration to allow credentials in requests

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,8 @@ const app = express();
 app.use(cors({
     origin: ['http://localhost:5174', 'https://restaurant-frontend-mocha.vercel.app'], // Allow all domains for now
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow credentials (cookies, authorization headers)
 }));
 
 // Database connection
