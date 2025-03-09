c16e0d1a70e5227d46923c9544cd32c0b7f35c91
Govindu <govinduthejana@gmail.com>
2025-03-09 18:52:59 +0530

    Update favicon to use PNG format and change frontend title

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import itemRouter from './routes/itemRouter.js';
 import userRouter from './routes/userRouter.js';
@@ -18,7 +17,7 @@ const mongoURI = process.env.mongoDBURL;
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5174', 'https://restaurant-frontend-mocha.vercel.app'], // Allow all domains for now
+    origin: "*", // Allows all origins (NOT SECURE)
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials (cookies, authorization headers)
@@ -29,7 +28,6 @@ mongoose.connect(mongoURI)
     .then(() => console.log("Connected to database"))
     .catch(err => console.error("Database connection error:", err));
 
-app.use(bodyParser.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
